5e1ebafe6d7d93fde65e17b40e949c8bbf8ecc30
Emiliano Rojas@ <inversiones.socony@gmail.com>
2023-12-05 22:58:05 -0700

    categories crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db.tmnuzfepvjtuxvndewwo.supabase.co',
+      port: 5432,
+      username: 'postgres',
+      password: 'your_password',
+      database: 'postgres',
+      entities: [/* your entities here */],
+      synchronize: true,
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
